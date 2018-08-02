f21116d4e36772aed49601888017080f754e320a
root <root@master1.k8s>
2018-08-02 11:23:54 +0800

    remove import pkg hexutil
    Signed-off-by: root <root@master1.k8s>

--- tokenutil/tokenutil.go
+++ tokenutil/tokenutil.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 )
